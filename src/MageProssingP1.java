83903878155a13da30eb6eaadc4b7a18c31743dd
moltenpopsicle <brennan.wolters@gmail.com>
2017-01-12 19:26:01 -0600

    Added ability to foreach over directory, isolated green rectangles, majorly optimized code

--- src/MageProssingP1.java
+++ src/MageProssingP1.java
@@ -9,29 +9,29 @@ public class MageProssingP1 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		BufferedImage image = ImageIO.read(new File("C:\\Users\\Team1091\\Desktop\\Cam_Stamples\\Good\\WIN_20170111_18_48_39_Pro.jpg"));
-		BufferedImage testImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
-		int[][][] screen = new int[image.getWidth()][image.getHeight()][3];
+		String dir = "C:\\Users\\Team1091\\Desktop\\PhotoSamplesSpike_2017\\Good";
+		File directory = new File(dir);
+		File[] flist = directory.listFiles();
+		dir += "_PROCESSED";
+		directory = new File(dir);
+		directory.mkdir();
+		for (int i = 0; i < flist.length; i++) {
+			BufferedImage image = ImageIO.read(flist[i]);
+			BufferedImage testImage = new BufferedImage(image.getWidth(), image.getHeight(),
+					BufferedImage.TYPE_INT_RGB);
 
-		for (int x = 0 ; x<image.getWidth(); x++){
-			for (int y = 0 ; y<image.getHeight(); y++){
-				Color color = new Color(image.getRGB(x, y));
-				screen[x][y][0] = color.getRed();
-				screen[x][y][1] = color.getGreen();
-				screen[x][y][2] = color.getBlue();
-			}	
-		}
+			for (int x = 0; x < image.getWidth(); x++) {
+				for (int y = 0; y < image.getHeight(); y++) {
 
-		for (int x = 0 ; x<image.getWidth(); x++){
-			for (int y = 0 ; y<image.getHeight(); y++){
-				Color color = new Color(0, screen[x][y][1], 0);
-				testImage.setRGB(x, y, color.getRGB());
-				
-			}	
+					if (new Color(image.getRGB(x, y)).getGreen() > 250) {
+						testImage.setRGB(x, y, 0x00FF00);
+					} else {
+						testImage.setRGB(x, y, 0x000000);
+					}
+				}
+			}
+
+			ImageIO.write(testImage, "PNG", new File(dir + "\\" + flist[i].getName()));
 		}
-		
-		
-		ImageIO.write(testImage, "PNG", new File("C:\\Users\\Team1091\\Desktop\\Cam_Stamples\\temp.png"));
 	}
-
 }
