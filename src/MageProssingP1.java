315b8b010f90ac8faa002b74f24f982dbf83b988
moltenpopsicle <brennan.wolters@gmail.com>
2017-01-14 09:49:35 -0600

    add average dot algorithum

--- src/MageProssingP1.java
+++ src/MageProssingP1.java
@@ -2,6 +2,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 import javax.imageio.ImageIO;
 
@@ -19,18 +20,35 @@ public class MageProssingP1 {
 			BufferedImage image = ImageIO.read(flist[i]);
 			BufferedImage testImage = new BufferedImage(image.getWidth(), image.getHeight(),
 					BufferedImage.TYPE_INT_RGB);
+			long xsum = 0;
+			long ysum = 0;
+			int totalcount = 0;
 
 			for (int x = 0; x < image.getWidth(); x++) {
 				for (int y = 0; y < image.getHeight(); y++) {
 
 					if (new Color(image.getRGB(x, y)).getGreen() > 250) {
 						testImage.setRGB(x, y, 0x00FF00);
+						xsum += x;
+						ysum += y;
+						totalcount++;
 					} else {
 						testImage.setRGB(x, y, 0x000000);
 					}
 				}
 			}
+			int xcenter = (int) (xsum / totalcount);
+			int ycenter = (int) (ysum / totalcount);
 
+			testImage.setRGB(xcenter, ycenter, 0xff);
+			Graphics2D g = testImage.createGraphics();
+			g.setColor(Color.BLUE);
+			g.fillRect(xcenter - 5, ycenter - 5, 10, 10);
+
+			// F = P * D * (1/W)
+			// (F * W) / P = D
+			// F = 35p * 60in * (1/2in)
+			// F = 1050 p
 			ImageIO.write(testImage, "PNG", new File(dir + "\\" + flist[i].getName()));
 		}
 	}
